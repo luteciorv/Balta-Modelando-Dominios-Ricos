78ba488de7ff053b3b6238d47d3ae82cf648bd8d
Luís Felipe <luisrodrigues@bne.com.br>
2023-12-19 12:53:42 -0300

    Adição de queries

---
+++ PaymentContext.Domain/Queries/StudentQueries.cs
@@ -0,0 +1,10 @@
+﻿using PaymentContext.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace PaymentContext.Domain.Queries;
+
+public static class StudentQueries
+{
+    public static Expression<Func<Student, bool>> GetStudent(string document) =>
+        s => s.Document.Number == document;
+}
