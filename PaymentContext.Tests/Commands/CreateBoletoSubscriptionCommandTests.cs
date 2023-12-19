d910d376cdfe838d13bea09b44e3070226773278
Luís Felipe <luisrodrigues@bne.com.br>
2023-12-19 11:21:44 -0300

    Criação de teste para o CreateBoletoSubscriptionCommand

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,21 @@
+﻿using PaymentContext.Domain.Commands;
+
+namespace PaymentContext.Tests.Commands;
+
+[TestClass]
+public class CreateBoletoSubscriptionCommandTests
+{
+    [TestMethod]
+    public void Deve_retornar_erro_quando_nome_estiver_invalido()
+    {
+        var command = new CreateBoletoSubscriptionCommand
+        {
+            FirstName = "",
+            LastName = ""
+        };
+
+        command.Validate();
+
+        Assert.IsFalse(command.IsValid);
+    }
+}
